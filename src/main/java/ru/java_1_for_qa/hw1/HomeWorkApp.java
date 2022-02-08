f3b651e372821324b8970d580867213807c32679
kate <2951525@gmail.com>
2022-02-09 00:17:22 +0300

    Commit 2: Выполнены пункты здания 4, 5, 6. Cозданы методы и все методы вызваны из метода main.

--- src/main/java/ru/java_1_for_qa/hw1/HomeWorkApp.java
+++ src/main/java/ru/java_1_for_qa/hw1/HomeWorkApp.java
@@ -4,6 +4,8 @@ public class HomeWorkApp {
     public static void main(String[] args) {
         printThreeWords();
         checkSumSign();
+        printColor();
+        compareNumbers();
     }
     //region Task2
     public static void printThreeWords(){
@@ -24,6 +26,28 @@ public class HomeWorkApp {
         }
     }
     //endregion
-
+    //region Task4
+    public static void printColor() {
+        int value = (int) Math.random();
+        if (value <=0) {
+            System.out.println("Красный");
+        } else if (value<=100){
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+    //endregion
+    //region Task5
+    public static void compareNumbers() {
+        int a = (int) Math.random();
+        int b = (int) Math.random();
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+    //endregion
 
 }
